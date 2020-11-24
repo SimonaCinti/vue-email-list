d92da025ec8f0a8aa8363da49041f596346f76a4
Simona Cinti <simona.cinti2@gmail.com>
2020-11-24 08:08:06 +0100

    clean js

--- js/main.js
+++ js/main.js
@@ -14,13 +14,7 @@ var app = new Vue({
     created(){
         //Declare .this
         const self = this;
-        // Call datas for array emails
-        for (let i = 0; i < 9; i++){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(function (response) {
-               return self.emails.push(response.data.response)
-            })
-        };
+        this.printList();
         //Call cats
         axios.get('https://aws.random.cat/meow')
             .then(function (response) {
@@ -34,8 +28,8 @@ var app = new Vue({
     },
 
     methods: {
-        // Print again the list
-        printAgain(){
+        // Print the list
+        printList(){
             console.log('click');
             //Declare .this
             const self = this;
